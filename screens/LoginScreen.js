c976b6a9bd3e0c1993974dbdcf2099d797572795
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-07 17:27:05 +1100

    feat: add login handler

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,29 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthnticating] = useState();
+
+  async function loginHandler({ email, password }) {
+    setIsAuthnticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication Failed!!",
+        "Please check your credentials or try again later"
+      );
+    }
+    setIsAuthnticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
