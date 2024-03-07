d501f2e514ed8a84c6d369035695d4af5b985d4f
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-07 17:03:27 +1100

    feat: create new user on signup, update .env, add plugin config to babel

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthnticating] = useState();
+
+  async function signupHandler({ email, password }) {
+    setIsAuthnticating(true);
+    await createUser(email, password);
+    setIsAuthnticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating User..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { API_KEY } from "@env";
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
+  console.log(API_KEY);
   const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
-      process.env.API_KEY,
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
       email: email,
       password: password,
