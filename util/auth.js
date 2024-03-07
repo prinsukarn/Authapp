562d0da597bab96f1fd785dbb59a77c98411f3c7
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-07 17:18:52 +1100

    feat: add login request, refactor login and signup request

--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { API_KEY } from "@env";
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+}
+
 export async function createUser(email, password) {
-  console.log(API_KEY);
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
