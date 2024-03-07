eab07195087d4076b048e9409682ce4f8c986826
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-07 17:43:27 +1100

    feat: get token from response and set token

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,16 +1,19 @@
-import { useState } from "react";
-import AuthContent from "../components/Auth/AuthContent";
+import { useContext, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 import { createUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { Alert } from "react-native";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthnticating] = useState();
+  const authCtx = useContext(AuthContext);
 
   async function signupHandler({ email, password }) {
     setIsAuthnticating(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      // set token
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication Failed!!",
