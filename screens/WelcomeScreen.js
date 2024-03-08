5b6ae07b707897bb295f804d4b409542db0893a4
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-08 20:33:51 +1100

    feat: fetch protected message from firebase

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://authentication-rn-eb897-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
